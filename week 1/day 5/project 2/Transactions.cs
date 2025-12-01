3129c1735fac88c44aa5213de9fc98d2e5a9da94
jake . <mouadtrafeh14@gmail.com>
2025-12-01 09:55:28 +0100

    update transactions.cs done

--- week 1/day 5/project 2/Transactions.cs
+++ week 1/day 5/project 2/Transactions.cs
@@ -17,5 +17,34 @@ public class Program
         int id = transactions.Count + 1;
         transactions.Add(new Transactions(id,title,amount,categorycategory,date));
     }
+    static void showTransactions()
+    {
+        foreach (var tr in transactions)
+        {
+            Console.WriteLine($"ID: {tr.id}, Title: {tr.title}, Amount: {tr.price}, Category: {tr.category}, Date: {tr.Date.ToShortDateString()}");
+        }
+
+    }
+    static void updateTransaction()
+    {
+        Console.WriteLine("enter the id of the transaction to update");
+        int id = int.Parse(Console.ReadLine());
+        var tr = transactions.Find(t => t.id == id);
+        if (tr != null)
+        {
+            Console.WriteLine("enter new title");
+            tr.title = Console.ReadLine();
+            Console.WriteLine("enter new amount");
+            tr.price = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("enter new category");
+            tr.category = Console.ReadLine();
+            Console.WriteLine("enter new date (yyyy-mm-dd)");
+            tr.Date = DateTime.Parse(Console.ReadLine());
+        }
+        else
+        {
+            Console.WriteLine("transaction not found");
+        }
+    }
     
 }
