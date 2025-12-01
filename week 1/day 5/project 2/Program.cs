8897d16101486b6d4a0b1370df3be7da80d9c44d
jake . <mouadtrafeh14@gmail.com>
2025-12-01 09:41:26 +0100

    Transactions class created

---
+++ week 1/day 5/project 2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+class Transactions
+{
+    public int id;
+    public string title;
+    public decimal price;
+    public string category;
+    public DateTime Date;
+public Transactions(int id, string title, decimal price, string category, DateTime date)
+{
+    this.id = id;
+    this.title = title;
+    this.price = price;
+    this.category = category;
+    this.Date = date;
+}
+}
